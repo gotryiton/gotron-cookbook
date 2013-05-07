0ac0f7c191b70ad286d765c275e9c534a1522dc7
Scott Bader <sb@scottbader.org>
2013-05-07 13:20:30 -0400

    fixing image_deletion with rbenv

--- recipes/image_deletion_cron.rb
+++ recipes/image_deletion_cron.rb
@@ -3,7 +3,7 @@ environment = node[:gotron][:app_environment]
 cron "image_deletion" do
   hour '5'
   minute '0'
-  command "cd /var/www/ootd/current && /usr/local/bin/rake RAILS_ENV=#{environment} db:cleanup:images 2>&1 | logger -t '##IMAGE_DELETION##'"
+  command "cd /var/www/ootd/current && bin/rake RAILS_ENV=#{environment} db:cleanup:images 2>&1 | logger -t '##IMAGE_DELETION##'"
   user node[:gotron][:cron_user]
 end
 

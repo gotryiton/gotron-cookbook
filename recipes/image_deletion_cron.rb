5d3d8c7e7a3ad5d493586152ad51d900eaa0005f
Scott Bader <sb@scottbader.org>
2013-05-07 14:01:11 -0400

    fixing cron path

--- recipes/image_deletion_cron.rb
+++ recipes/image_deletion_cron.rb
@@ -4,7 +4,7 @@ cron "image_deletion" do
   hour '5'
   minute '0'
   command "cd /var/www/ootd/current && bin/rake RAILS_ENV=#{environment} db:cleanup:images 2>&1 | logger -t '##IMAGE_DELETION##'"
-  path "/home/deployer/.rbenv/shims:/home/deployer/.rbenv/bin"
+  path "/home/vagrant/.rbenv/shims:/home/vagrant/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
   user "deployer"
 end
 

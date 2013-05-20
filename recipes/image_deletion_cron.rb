e2176db27157a0bf7b89cf879de5dbc06b7b9fb6
Scott Bader <sb@scottbader.org>
2013-05-20 16:15:24 -0400

    fixing cron

--- recipes/image_deletion_cron.rb
+++ recipes/image_deletion_cron.rb
@@ -1,10 +1,11 @@
 environment = node[:gotron][:app_environment]
 
-cron "image_deletion" do
-  hour '5'
-  minute '0'
-  command "cd /var/www/ootd/current && bin/rake RAILS_ENV=#{environment} db:cleanup:images 2>&1 | logger -t '##IMAGE_DELETION##'"
-  path "/home/vagrant/.rbenv/shims:/home/vagrant/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-  user "deployer"
+if node[:gotron][:variant] != "local"
+  cron "image_deletion" do
+    hour '5'
+    minute '0'
+    command "cd /var/www/ootd/current && bin/rake RAILS_ENV=#{environment} db:cleanup:images 2>&1 | logger -t '##IMAGE_DELETION##'"
+    path "/home/vagrant/.rbenv/shims:/home/vagrant/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    user "deployer"
+  end
 end
-
